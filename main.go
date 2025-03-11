3756a69d8a5ca05de8056bb5c080db38a2d05649
rayhankinan <rayhankinan@gmail.com>
2025-03-11 18:40:55 +0700

    Feat: Add Redis Client

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -8,12 +10,13 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/r3labs/sse/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	streamID := "time"
+	ctx := context.Background()
+	streamID := "pubsub"
 	eventTTL := 5 * time.Second
-	eventInterval := 1 * time.Second
 
 	e := echo.New()
 	defer e.Close()
@@ -22,29 +25,57 @@ func main() {
 	server.EventTTL = eventTTL
 	defer server.Close()
 
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
 	server.CreateStream(streamID)
 
-	go func(s *sse.Server, streamID string, eventInterval time.Duration) {
-		ticker := time.NewTicker(eventInterval)
-		defer ticker.Stop()
-		for range ticker.C {
-			s.Publish(streamID, &sse.Event{
-				Data: fmt.Appendf(nil, "Time: %s", time.Now().Format(time.RFC3339Nano)),
-			})
-		}
-	}(server, streamID, eventInterval)
+	go func() {
+		pubsub := rdb.Subscribe(ctx, streamID)
+		defer pubsub.Close()
 
-	e.GET("/sse", func(c echo.Context) error {
-		e.Logger.Printf("The client is connected: %v\n", c.RealIP())
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case message := <-pubsub.Channel():
+				server.Publish(streamID, &sse.Event{
+					Data: []byte(message.Payload),
+				})
+			}
+		}
+	}()
+
+	e.GET("/subscribe", func(c echo.Context) error {
+		e.Logger.Infof("The client is connected: %v\n", c.RealIP())
 
 		go func() {
 			<-c.Request().Context().Done()
-			e.Logger.Printf("The client is disconnected: %v\n", c.RealIP())
+			e.Logger.Infof("The client is disconnected: %v\n", c.RealIP())
 		}()
 
 		server.ServeHTTP(c.Response(), c.Request())
 
 		return nil
+	})
+
+	e.POST("/publish", func(c echo.Context) error {
+		var data any
+		if err := c.Bind(&data); err != nil {
+			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error while reading the body: %s", err))
+		}
+
+		minifiedJSON, err := json.Marshal(data)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error while minifying the body: %s", err))
+		}
+
+		if err := rdb.Publish(ctx, streamID, string(minifiedJSON)).Err(); err != nil {
+			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error while publishing: %s", err))
+		}
+
+		return c.JSON(http.StatusOK, "Published")
 	})
 
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
